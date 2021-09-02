b77b2c69e964f424b99976dc369c97533b54a748
Vitor Oliveira <vmoliveira91@gmail.com>
2021-09-01 22:54:48 -0300

    Fix desafio02/exercicio02

--- desafio02/src/main.js
+++ desafio02/src/main.js
@@ -1,15 +1,15 @@
+import { useState } from 'react'
+
 import Heading from './heading'
 
-const Main = () => {
+const Main = ({ titleProps, contentProps }) => {
+    const [title, setTitle] = useState(titleProps)
+    const [content, setContent] = useState(contentProps)
+
     return (
         <main className="main container">
-            <Heading type="2">Conteúdo</Heading>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vehicula nunc a porta condimentum. Cras placerat pretium ullamcorper. Quisque vestibulum placerat massa, nec aliquet dolor. Curabitur tellus nunc, ultricies eu lacinia et, tincidunt ac lectus. Suspendisse pulvinar rhoncus odio nec porta. Pellentesque augue justo, interdum et feugiat vitae, interdum vel elit. Cras suscipit mi et ex lacinia, quis consectetur mi mattis. Morbi nec ligula mi. Sed sit amet malesuada mauris, finibus euismod eros.</p>
-            <ul>
-                <li>Quisque fringilla</li>
-                <li>Suspendisse potenti</li>
-                <li>Vestibulum ante ipsum</li>
-            </ul>
+            <Heading type="2">{title}</Heading>
+            {content}
         </main>
     )
 }
